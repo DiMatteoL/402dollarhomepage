38f359135e2f6894d71bca155ad31603d5749acc
DiMatteoL <luca7dimatteo@gmail.com>
2025-12-14 17:00:09 +0100

    g41

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -140,6 +140,19 @@ export default function RootLayout({
             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           `}
         </Script>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-46RJWFJTKT"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-46RJWFJTKT');
+          `}
+        </Script>
       </head>
       <body className="min-h-screen overflow-hidden font-sans antialiased">
         <PreventBrowserZoom />
